9e6cc34c556a47bf4bf236cb995104e133c7aa47
Benjent <Benjent@hotmail.fr>
2018-05-04 21:51:43 +0200

    Hide player for now

--- js/main.js
+++ js/main.js
@@ -34,8 +34,8 @@ var indexVue = new Vue({
       // Select
       let clickedId = event.target.id
       this.selectedAlbum = getAlbumById(clickedId);
-      player.loadVideoById(this.selectedAlbum.selectedTrackYtId);
-      player.stopVideo()
+      // player.loadVideoById(this.selectedAlbum.selectedTrackYtId);
+      // player.stopVideo()
       // Randomize
       this.randomAlbums = getRandomAlbumsByLength(numberOfAlbumsInRandomVue, clickedId).slice(0);
     },
@@ -46,8 +46,8 @@ var indexVue = new Vue({
       }
       this.selectedAlbum = albums[albumId];
       selectedAlbumId = albumId;
-      player.loadVideoById(this.selectedAlbum.selectedTrackYtId);
-      player.stopVideo()
+      // player.loadVideoById(this.selectedAlbum.selectedTrackYtId);
+      // player.stopVideo()
     }
   },
   computed: {
@@ -72,39 +72,39 @@ function randomize() {
   return Math.floor(Math.random() * (max - min)) + min; // Randomize id
 }
 
-// Youtube Player
+// // Youtube Player
 
-// Load the IFrame Player API code asynchronously
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// // Load the IFrame Player API code asynchronously
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// Create <iframe> (and YouTube player) after the API code downloads
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '70',
-    width: '300',
-    videoId: indexVue.selectedAlbum.selectedTrackYtId,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
+// // Create <iframe> (and YouTube player) after the API code downloads
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '70',
+//     width: '300',
+//     videoId: indexVue.selectedAlbum.selectedTrackYtId,
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+// }
 
-function onPlayerReady(event) {
-//   event.target.playVideo();
-}
+// function onPlayerReady(event) {
+// //   event.target.playVideo();
+// }
 
-// Video is being played <=> state=1
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo()
-}
+// // Video is being played <=> state=1
+// var done = false;
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     done = true;
+//   }
+// }
+// function stopVideo() {
+//   player.stopVideo()
+// }
