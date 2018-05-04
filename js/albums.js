0acee514ae391e4fe909c526b3a3243f792692d2
Benjent <Benjent@hotmail.fr>
2018-05-04 22:08:38 +0200

    Use a YT link for now

--- js/albums.js
+++ js/albums.js
@@ -10,7 +10,7 @@ const albums = [
         "cover": "img/covers/alpha_ralpha_alpha_ralpha.jpg",
         "criteria": [6, 11, 14, 19],
         "selectedTrackTitle": "Genèse",
-        "selectedTrackYtId": "jTS5f1nSurQ"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=jTS5f1nSurQ"
     },
     {
         "id": "badger_one_live_badger",
@@ -21,7 +21,7 @@ const albums = [
         "cover": "img/covers/badger_one_live_badger.jpg",
         "criteria": [0, 3, 7, 12, 22],
         "selectedTrackTitle": "Fountain",
-        "selectedTrackYtId": "To2NXcCRC58"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=To2NXcCRC58"
     },
     {
         "id": "camel_mirage",
@@ -32,7 +32,7 @@ const albums = [
         "cover": "img/covers/camel_mirage.jpg",
         "criteria": [3, 8, 11, 42],
         "selectedTrackTitle": "Freefall",
-        "selectedTrackYtId": "Gcagz8epRN8"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=Gcagz8epRN8"
     },
     {
         "id": "camel_moonmadness",
@@ -43,7 +43,7 @@ const albums = [
         "cover": "img/covers/camel_moonmadness.jpg",
         "criteria": [3, 11, 19],
         "selectedTrackTitle": "Air Born",
-        "selectedTrackYtId": "JR4aCz-JZrY"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=JR4aCz-JZrY"
     },
     {
         "id": "captain_beyond_captain_beyond",
@@ -54,7 +54,7 @@ const albums = [
         "cover": "img/covers/captain_beyond_captain_beyond.jpg",
         "criteria": [2, 3, 9, 40, 41],
         "selectedTrackTitle": "As The Moon Speaks (Part I)",
-        "selectedTrackYtId": "wm61ROOZ5ek"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=wm61ROOZ5ek"
     },
     {
         "id": "focus_moving_waves",
@@ -65,7 +65,7 @@ const albums = [
         "cover": "img/covers/focus_moving_waves.jpg",
         "criteria": [6, 8, 10, , 11, 19, 35],
         "selectedTrackTitle": "Janis",
-        "selectedTrackYtId": "vx-JzRzsCss"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=vx-JzRzsCss"
     },
     {
         "id": "focus_focus_3",
@@ -76,7 +76,7 @@ const albums = [
         "cover": "img/covers/focus_focus_3.jpg",
         "criteria": [3, 4, 6, 7, 8, 19, 35, 42],
         "selectedTrackTitle": "Sylvia",
-        "selectedTrackYtId": "aig4mWJUpLw"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=aig4mWJUpLw"
     },
     {
         "id": "frank_zappa_hot_rats",
@@ -87,7 +87,7 @@ const albums = [
         "cover": "img/covers/frank_zappa_hot_rats.jpg",
         "criteria": [6, 7, 15, 23, 35, 42],
         "selectedTrackTitle": "Peaches En Regalia",
-        "selectedTrackYtId": "zprYyWMcUGY"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=zprYyWMcUGY"
     },
     {
         "id": "frank_zappa_apostrophe",
@@ -98,7 +98,7 @@ const albums = [
         "cover": "img/covers/frank_zappa_apostrophe.jpg",
         "criteria": [2, 3, 9, 17, 22, 35],
         "selectedTrackTitle": "Father O'blivion",
-        "selectedTrackYtId": "2XO7xSwYVZI"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=2XO7xSwYVZI"
     },
     {
         "id": "jade_warrior_waves",
@@ -109,7 +109,7 @@ const albums = [
         "cover": "img/covers/jade_warrior_waves.jpg",
         "criteria": [1, 6, 8, 9, 11, 13, 14, 19],
         "selectedTrackTitle": "Waves (Part II)",
-        "selectedTrackYtId": "uGYpyLxDzlk"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=uGYpyLxDzlk"
     },
     {
         "id": "pink_floyd_dark_side_of_the_moon",
@@ -120,7 +120,7 @@ const albums = [
         "cover": "img/covers/pink_floyd_dark_side_of_the_moon.jpg",
         "criteria": [1, 3, 9, 11, 16, 20],
         "selectedTrackTitle": "Time",
-        "selectedTrackYtId": "rL3AgkwbYgo"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=rL3AgkwbYgo"
     },
     {
         "id": "pink_floyd_the_wall",
@@ -131,7 +131,7 @@ const albums = [
         "cover": "img/covers/pink_floyd_the_wall.jpg",
         "criteria": [1, 9, 16, 17, 30, 43],
         "selectedTrackTitle": "The Trial",
-        "selectedTrackYtId": "4fa7AtI1msk"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=4fa7AtI1msk"
     },
     {
         "id": "shakti_natural_elements",
@@ -142,7 +142,7 @@ const albums = [
         "cover": "img/covers/shakti_natural_elements.jpg",
         "criteria": [1, 3, 4, 6, 7, 13, 15, 19, 21],
         "selectedTrackTitle": "Mind Ecology",
-        "selectedTrackYtId": "d_DajSHROJA"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=d_DajSHROJA"
     },
     {
         "id": "supertramp_crime_of_the_century",
@@ -153,7 +153,7 @@ const albums = [
         "cover": "img/covers/supertramp_crime_of_the_century.jpg",
         "criteria": [1, 2, 11, 17, 19],
         "selectedTrackTitle": "Rudy",
-        "selectedTrackYtId": "gxW3s_Qk1dk"
+        "selectedTrackYtId": "https://www.youtube.com/watch?v=gxW3s_Qk1dk"
     }
 
     // {
@@ -165,7 +165,7 @@ const albums = [
     //     "cover": "img/covers/frank_zappa_one_size_fits_all.jpg",
     //     "criteria": [2, 6, 11, 18, 21, 29],
     //     "selectedTrackTitle": "Fountain",
-    //     "selectedTrackYtId": "To2NXcCRC58"
+    //     "selectedTrackYtId": "https://www.youtube.com/watch?v=To2NXcCRC58"
     // },
     // 
 ]
