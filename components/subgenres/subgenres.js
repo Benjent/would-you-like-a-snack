ca17eabdc675deef508baebd6cf0c0a4a165d7c8
Benjent <Benjent@hotmail.fr>
2018-09-18 19:08:00 +0200

    Create subgenre vue

---
+++ components/subgenres/subgenres.js
@@ -0,0 +1,78 @@
+Vue.component('subgenres', {
+    template: `
+        <section id="subgenresVue">
+            <div class="subgenre-wrapper">
+                <span>Canterbury School</span>
+                Caravan
+            </div>
+            <div class="subgenre-wrapper">
+                <span>New Age</span>
+                Zanov, Tangerine Dream
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Jazz</span>
+                Gong, Zappa
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Art Pop</span>
+                Supertramp
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Symphonic</span>
+                Yes
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Avant-Prog</span>
+                Henry Cow, Area
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Psychedelic</span>
+                Zappa, Can
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Electro Rock</span>
+                Elo, Alan Parsons
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Eclectic</span>
+                Bubu, Gentle Giant
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Zeuhl</span>
+                Magma, Weidorje
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Heavy</span>
+                Uriah Heep, Rainbow
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Folk</span>
+                Spifogyra
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Renaissance</span>
+                Semiramis
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Space Rock</span>
+                Hawkwind, Ozric
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Opera/Fanfare</span>
+                Queen, Arthur Brown, Jesus CHrist SUperstar, Butteffly ball
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Neo-Prog</span>
+                Marillion
+            </div>
+            <div class="subgenre-wrapper">
+                <span>Fantasy</span>
+                paint a picture, book of talyesin
+            </div>
+        </section>
+    `,
+    data: function () {
+        return {
+        }
+    }
+})
