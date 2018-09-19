a30e9a69b35ec34a2c3f32c393f38b5f8f8a73c7
Benjent <Benjent@hotmail.fr>
2018-09-19 18:48:43 +0200

    Add albums

--- db/subgenres.js
+++ db/subgenres.js
@@ -4,7 +4,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("supertramp_crime_of_the_century"),
         "albums": [
-
+            getAlbumById("peter_gabriel_car"),
+            getAlbumById("david_bowie_the_rise_and_fall_of_ziggy_stardust_and_the_spiders_from_mars"),
+            getAlbumById("the_doors_the_soft_parade"),
+            getAlbumById("raymond_vincent_metronomics")
         ]
     },
     {
@@ -12,7 +15,9 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("henry_cow_legend"),
         "albums": [
-            getAlbumById("area_arbeit_macht_frei")
+            getAlbumById("area_arbeit_macht_frei"),
+            getAlbumById("faust_faust"),
+            getAlbumById("baba_yaga_collage")
         ]
     },
     {
@@ -28,9 +33,11 @@ const subgenres = [
     {
         "name": "Eclectic",
         "description": "TODO",
-        "mostRepresentativeAlbum": getAlbumById("bubu_anabelas"),
+        "mostRepresentativeAlbum": getAlbumById("gentle_giant_acquiring_the_taste"),
         "albums": [
-            getAlbumById("gentle_giant_acquiring_the_taste")
+            getAlbumById("king_crimson_red"),
+            getAlbumById("alpha_ralpha_alpha_ralpha"),
+            getAlbumById("jade_warrior_way_of_the_sun")
         ]
     },
     {
@@ -38,6 +45,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("eloy_ocean"),
         "albums": [
+            getAlbumById("electric_light_orchestra_time"),
+            getAlbumById("asia_asia"),
+            getAlbumById("happy_the_man_happy_the_man"),
+            getAlbumById("ml_bongers_project_pacific_prison")
         ]
     },
     {
@@ -45,7 +56,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("deep_purple_the_book_of_taliesyn"),
         "albums": [
+            getAlbumById("the_moody_blues_days_of_future_passed"),
+            getAlbumById("the_beatles_yellow_submarine")
             // Fantasy paint a picture
+            // Procol Harum
         ]
     },
     {
@@ -56,11 +70,24 @@ const subgenres = [
         ]
     },
     {
+        "name": "Glitter",
+        "description": "TODO",
+        "mostRepresentativeAlbum": getAlbumById("windchase_symphinity"),
+        "albums": [
+            getAlbumById("starcastle_starcastle"),
+            getAlbumById("yes_going_for_the_one"),
+            getAlbumById("nightwinds_nightwinds"),
+            getAlbumById("ramses_light_fantastic")
+        ]
+    },
+    {
         "name": "Heavy",
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("uriah_heep_demons_and_wizards"),
         "albums": [
-            // "rainbow"
+            getAlbumById("rainbow_rising"),
+            getAlbumById("winterhawk_revival"),
+            getAlbumById("badger_one_live_badger")
         ]
     },
     {
@@ -68,8 +95,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("frank_zappa_hot_rats"),
         "albums": [
-            // "weather report",
-            // "gong"
+            getAlbumById("return_to_forever_romantic_warrior"),
+            getAlbumById("weather_report_black_market"),
+            getAlbumById("gong_gazeuse"),
+            getAlbumById("herbie_hancock_thrust")
         ]
     },
     {
@@ -77,8 +106,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("jean_michel_jarre_oxygene"),
         "albums": [
-            // "zanov",
-            // "tangerine dream"
+            getAlbumById("zanov_in_course_of_time"),
+            getAlbumById("tangerine_dream_phaedra"),
+            getAlbumById("ralph_lundsten_alpha_ralpha_boulevard"),
+            getAlbumById("anna_sjalv_tredje_tussilago_fanfara")
         ]
     },
     {
@@ -86,7 +117,8 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("marillion_script_for_a_jesters_tear"),
         "albums": [
-            // "king crimson"
+            getAlbumById("king_crimson_three_of_a_perfect_pair"),
+            getAlbumById("low_flying_aircraft_low_flying_aircraft")
         ]
     },
     {
@@ -94,8 +126,9 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("roger_glover_the_butterfly_ball_and_the_grasshoppers_feast"),
         "albums": [
-            getAlbumById("queen_queen_II")
-            // "arthur brown",
+            getAlbumById("queen_queen_II"),
+            getAlbumById("the_crazy_world_of_arthur_brown_the_crazy_world_of_arthur_brown"),
+            getAlbumById("frank_zappa_joes_garage")
             // "jesus christ superstar"
         ]
     },
@@ -104,8 +137,11 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("gong_flying_teapot"),
         "albums": [
-            // "zappa",
-            // "can"
+            getAlbumById("can_tago_mago"),
+            getAlbumById("sweet_smoke_just_a_poke"),
+            getAlbumById("frank_zappa_were_only_in_it_for_the_money"),
+            getAlbumById("amon_duul_paradieswarts_duul"),
+            getAlbumById("the_incredible_string_band_the_5000_spirits_or_the_layers_of_the_onion")
         ]
     },
     {
@@ -128,7 +164,10 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("yes_close_to_the_edge"),
         "albums": [
-            // "starcastle"
+            getAlbumById("mike_oldfield_hergest_ridge"),
+            getAlbumById("bubu_anabelas"),
+            getAlbumById("van_der_graaf_generator_h_to_he_who_am_the_only_one"),
+            getAlbumById("emerson_lake_and_palmer_tarkus")
         ]
     },
     {
@@ -136,7 +175,7 @@ const subgenres = [
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("magma_mekanik_destruktiw_kommandoh"),
         "albums": [
-            // "weidorje"
+            getAlbumById("weidorje_weidorje")
         ]
     }
     
