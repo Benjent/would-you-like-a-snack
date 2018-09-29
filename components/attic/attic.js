23343582e6dd278d2002f1180c245978df6b2d31
Benjent <Benjent@hotmail.fr>
2018-09-29 14:23:55 +0200

    Filter works but poor perfs

--- components/attic/attic.js
+++ components/attic/attic.js
@@ -1,47 +1,103 @@
 Vue.component('attic', {
     template: `
         <section id="atticVue">
-            <div
-                class="album-wrapper"
-                v-for="album in albums"
-                v-bind:albumId="album.id"
-                v-on:click="$emit('album-click', album)">
 
+            <div id="filter">
                 <div
-                    class="album-infos"
-                    v-bind:album-id="album.id">
+                    class="criteria-wrapper"
+                    v-for="criterium in this.filter">
 
-                    <span
-                        class="album-artist"
-                        v-bind:album-id="album.id">
-                        {{album.artist}}
-                    </span>
-                    <span
-                        class="album-title"
-                        v-bind:album-id="album.id">
-                        {{album.title}}
-                    </span>
-                    <span
-                        class="album-year"
-                        v-bind:album-id="album.id">
-                        {{album.year}}
-                    </span>
-                    <span
-                        class="album-rating"
-                        v-if="album.isAGem"
-                        v-bind:album-id="album.id">
-                        This is a must-hear
-                    </span>
-                    
+                    <div
+                        class="checkbox-wrapper"
+                        v-on:click="toggleFilterCriterium($event.currentTarget, criterium.string)">
+
+                        <span class="checkbox-slot"></span>
+                        <label>{{criterium.string}}</label>
+
+                    </div>
+
                 </div>
-
-                <img class="album" v-bind:src=album.cover alt="">
             </div>
+
+            <template v-for="album in db.albums">
+                <template v-for="criterium in computedFilterArray">
+                    <template
+                        v-for="albumCriterium in album.criteria"
+                        v-if="albumCriterium == criterium.string && criterium.checked">
+
+                        <div
+                            class="album-wrapper"
+                            v-bind:albumId="album.id"
+                            v-on:click="$emit('album-click', album)">
+
+                            <div>
+                                {{albumCriterium}}
+                            </div>
+
+                            <div
+                                class="album-infos"
+                                v-bind:album-id="album.id">
+
+                                <span
+                                    class="album-artist"
+                                    v-bind:album-id="album.id">
+                                    {{album.artist}}
+                                </span>
+                                <span
+                                    class="album-title"
+                                    v-bind:album-id="album.id">
+                                    {{album.title}}
+                                </span>
+                                <span
+                                    class="album-year"
+                                    v-bind:album-id="album.id">
+                                    {{album.year}}
+                                </span>
+                                <span
+                                    class="album-rating"
+                                    v-if="album.isAGem"
+                                    v-bind:album-id="album.id">
+                                    This is a must-hear
+                                </span>
+                                
+                            </div>
+
+                            <img class="album" v-bind:src=album.cover alt="">
+                       
+                        </div>
+
+                    </template>
+                </template>
+            </template>
+
         </section>
     `,
+    props: ['db'],
     data: function () {
+
+        const initialFilter = {};
+
+        for(let i = 0; i < this.db.criteria.length; i++) {
+            initialFilter[this.db.criteria[i]] = {
+                "string": this.db.criteria[i],
+                checked: false
+            }
+        }
+
         return {
-            albums: albums,
+            filter: initialFilter
+        }
+    },
+    computed: {
+        computedFilterArray() {
+            return this.filter;
+        }
+    },
+    methods: {
+        toggleFilterCriterium: function(htmlWrapper, criteriumString) {
+            this.filter[criteriumString].checked = !this.filter[criteriumString].checked;
+
+            htmlWrapper.classList.contains("checked") ? htmlWrapper.classList.remove("checked") : htmlWrapper.classList.add("checked");
         }
     }
 })
