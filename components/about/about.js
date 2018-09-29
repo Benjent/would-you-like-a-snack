7620d3d6e0345e5ae735a5f8d00c0591561b52bb
Benjent <Benjent@hotmail.fr>
2018-09-29 23:21:06 +0200

    Reset style so that scrollbar starts below header

--- components/about/about.js
+++ components/about/about.js
@@ -2,32 +2,34 @@ Vue.component('about', {
     template: `
         <section id="aboutVue">
 
-            <h2>Hi!</h2>
-        
-            <div class="me-wrapper">
-                <img
-                    class="me"
-                    v-bind:src="mePath">
-                <div class="me-description">
-                    <span>The face of a sane man</span>
-                    <img
-                        class="arrow"
-                        v-bind:src="arrowPath">
-                </div>
-            </div>
+            <div class="content">
+                <h2>Hi!</h2>
             
-            <p>
-                Would You Like A Snack is a tiny web project with big music ambitions: dusting old prog albums, known as prog gems, to make them shine beside classic albums.
-            </p>
-            <p>
-                Prog albums are anything related to progressive music, from late 60's psychedelic vibes to early 80's electronic wave ; still focusing on the progressive bloom of the 70's.
-            </p>
-            <p>
-                This project is designed as a tool to discover albums, the way we would pick up vinyls in music stores, as well as centralizer of what can be found scrapping the web.
-            </p>
-            <p>
-                If you spot any wrong information or any typo, think of improvements, or would like to leave a sweet comment, please feel free to contact me at benjent@hotmail.fr.
-            </p>
+                <div class="me-wrapper">
+                    <img
+                        class="me"
+                        v-bind:src="mePath">
+                    <div class="me-description">
+                        <span>The face of a sane man</span>
+                        <img
+                            class="arrow"
+                            v-bind:src="arrowPath">
+                    </div>
+                </div>
+                
+                <p>
+                    Would You Like A Snack is a tiny web project with big music ambitions: dusting old prog albums, known as prog gems, to make them shine beside classic albums.
+                </p>
+                <p>
+                    Prog albums are anything related to progressive music, from late 60's psychedelic vibes to early 80's electronic wave ; still focusing on the progressive bloom of the 70's.
+                </p>
+                <p>
+                    This project is designed as a tool to discover albums, the way we would pick up vinyls in music stores, as well as centralizer of what can be found scrapping the web.
+                </p>
+                <p>
+                    If you spot any wrong information or any typo, think of improvements, or would like to leave a sweet comment, please feel free to contact me at benjent@hotmail.fr.
+                </p>
+            </div>
         </section>
     `,
     data: function () {
