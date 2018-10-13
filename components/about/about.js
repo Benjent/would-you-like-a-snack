9cf74ebe17ae9b730568132842e6d490c1187cbc
Benjent <Benjent@hotmail.fr>
2018-10-13 15:22:26 +0200

    Add 1965 to 1995 in about

--- components/about/about.js
+++ components/about/about.js
@@ -21,7 +21,7 @@ Vue.component('about', {
                     Would You Like A Snack is a tiny web project with big music ambitions: dusting old prog albums, known as prog gems, to make them shine beside classic albums.
                 </p>
                 <p>
-                    Prog albums are anything related to progressive music, from late 60's psychedelic vibes to early 80's electronic wave ; still focusing on the progressive bloom of the 70's.
+                    Prog albums are anything related to progressive music, from late 60's psychedelic vibes to early 80's electronic wave ; still focusing on the progressive bloom of the 70's and not going further than 1965-1995.
                 </p>
                 <p>
                     This project is designed as a tool to discover albums, the way we would pick up vinyls in music stores, as well as centralizer of what can be found scrapping the web.
