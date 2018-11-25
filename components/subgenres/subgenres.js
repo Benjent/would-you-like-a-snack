e1663265cc5c85735577850196b0d5989e470a01
Benjent <Benjent@hotmail.fr>
2018-11-25 20:14:05 +0100

    New subgenres display

--- components/subgenres/subgenres.js
+++ components/subgenres/subgenres.js
@@ -5,28 +5,37 @@ Vue.component('subgenres', {
                 class="subgenre-wrapper"
                 v-for="subgenre in subgenres">
 
+                <div class="subgenre-albums">
+                    <img
+                        class="album-cover"
+                        v-for="album in subgenre.albums"
+                        v-bind:album-id="album.id"
+                        v-on:click="$emit('album-click', album)"
+                        v-bind:src=album.cover alt="">
+                </div>
+
                 <img
                     class="album-cover most-representative"
                     v-bind:album-id="subgenre.mostRepresentativeAlbum.id"
                     v-on:click="$emit('album-click', subgenre.mostRepresentativeAlbum)"
                     v-bind:src=subgenre.mostRepresentativeAlbum.cover alt="">
 
-                <div
-                    class="subgenre-infos">
+                <div class="subgenre-infos">
 
-                    <span>{{subgenre.name}}</span>
+                    <span class="subgenre-name">{{subgenre.name}}</span>
+                    <p class="subgenre-criteria">
+                        Characterized by the following: 
+                        <span
+                            class="criterium"
+                            v-for="(criterium, index) in subgenre.criteria">
+                            {{criterium}}
+                            <span v-if="index < subgenre.criteria.length - 1">-</span>
+                        </span>
+                    </p>
                     <blockquote class="subgenre-description">{{subgenre.description}}</blockquote>
-                    
-                    <div class="albums">
-                        <img
-                            class="album-cover"
-                            v-for="album in subgenre.albums"
-                            v-bind:album-id="album.id"
-                            v-on:click="$emit('album-click', album)"
-                            v-bind:src=album.cover alt="">
-                    </div>
 
                 </div>
+
             </div>
         </section>
     `,
--- db/subgenres.js
+++ db/subgenres.js
@@ -7,8 +7,9 @@ const subgenres = [
             getAlbumById("peter_gabriel_car"),
             getAlbumById("david_bowie_the_rise_and_fall_of_ziggy_stardust_and_the_spiders_from_mars"),
             getAlbumById("the_doors_the_soft_parade"),
-            getAlbumById("raymond_vincent_metronomics")
-        ]
+            getAlbumById("kate_bush_never_for_ever")
+        ],
+        "criteria": [criteria.ART_POP, criteria.BLENDS, criteria.ROCK_OPERA, criteria.FANFARE, criteria.TOUCHING_VOCALS, criteria.ORCHESTRAL_PARTS, criteria.BAROQUE]
     },
     {
         "name": "Avant-Prog / Experimental",
@@ -19,7 +20,8 @@ const subgenres = [
             getAlbumById("faust_faust"),
             getAlbumById("baba_yaga_collage"),
             getAlbumById("soft_machine_third")
-        ]
+        ],
+        "criteria": [criteria.EXPERIMENTAL, criteria.INSTRUMENTAL, criteria.C_ELECTRO, criteria.C_PSYCHEDELIC, criteria.LENGTHY_TRACKS, criteria.SOUND_COLLAGES]
     },
     {
         "name": "Canterbury School / Folk Rock",
@@ -29,7 +31,8 @@ const subgenres = [
             getAlbumById("national_health_national_health"),
             getAlbumById("jethro_tull_thick_as_a_brick"),
             getAlbumById("camel_camel")
-        ]
+        ],
+        "criteria": [criteria.FOLK, criteria.C_FOLK, criteria.C_JAZZ, criteria.JAM, criteria.JAZZY_GROOVY, criteria.SOFT, criteria.FUNNY]
     },
     {
         "name": "Eclectic",
@@ -39,7 +42,8 @@ const subgenres = [
             getAlbumById("king_crimson_red"),
             getAlbumById("alpha_ralpha_alpha_ralpha"),
             getAlbumById("jade_warrior_way_of_the_sun")
-        ]
+        ],
+        "criteria": [criteria.CONCEPT, criteria.C_JAZZ, criteria.C_ELECTRO, criteria.ODD_RHYTHM, criteria.TECHNICAL, criteria.SOPHISTICATED, criteria.UNCOMMON_INSTRUMENTS]
     },
     {
         "name": "Electro Rock",
@@ -50,7 +54,8 @@ const subgenres = [
             getAlbumById("happy_the_man_happy_the_man"),
             getAlbumById("ml_bongers_project_pacific_prison"),
             getAlbumById("triumvirat_spartacus")
-        ]
+        ],
+        "criteria": [criteria.ELECTRO, criteria.C_ELECTRO, criteria.GLITTER, criteria.SCI_FI, criteria.SPECTACULAR]
     },
     {
         "name": "Fantasy / Baroque",
@@ -59,17 +64,19 @@ const subgenres = [
         "albums": [
             getAlbumById("the_moody_blues_days_of_future_passed"),
             getAlbumById("the_beatles_yellow_submarine"),
-            getAlbumById("pink_floyd_the_piper_at_the_gates_of_dawn")
-            // Fantasy paint a picture
+            getAlbumById("pink_floyd_the_piper_at_the_gates_of_dawn"),
+            getAlbumById("fantasy_paint_a_picture"),
             // Procol Harum
-        ]
+        ],
+        "criteria": [criteria.C_FOLK, criteria.C_ELECTRO, criteria.FANTASY, criteria.BAROQUE, criteria.ORCHESTRAL_PARTS, criteria.ART_POP, criteria.FANFARE]
     },
     {
         "name": "Folk",
         "description": "TODO",
         "mostRepresentativeAlbum": getAlbumById("spirogyra_old_boot_wine"),
         "albums": [
-        ]
+        ],
+        "criteria": [criteria.FOLK, criteria.FANTASY, criteria.SOFT, criteria.HUMBLE, criteria.C_PSYCHEDELIC]
     },
     {
         "name": "Glitter",
@@ -80,7 +87,8 @@ const subgenres = [
             getAlbumById("yes_going_for_the_one"),
             getAlbumById("nightwinds_nightwinds"),
             getAlbumById("ramses_light_fantastic")
-        ]
+        ],
+        "criteria": [criteria.GLITTER, criteria.SYMPHONIC, criteria.SPECTACULAR, criteria.C_ELECTRO]
     },
     {
         "name": "Heavy",
@@ -90,7 +98,8 @@ const subgenres = [
             getAlbumById("rainbow_rising"),
             getAlbumById("winterhawk_revival"),
             getAlbumById("badger_one_live_badger")
-        ]
+        ],
+        "criteria": [criteria.HEAVY, criteria.ROCK, criteria.JAM, criteria.FANTASY, criteria.ODD_RHYTHM]
     },
     {
         "name": "Jazz Rock / Jazz Fusion",
@@ -101,7 +110,8 @@ const subgenres = [
             getAlbumById("weather_report_black_market"),
             getAlbumById("gong_gazeuse"),
             getAlbumById("herbie_hancock_thrust")
-        ]
+        ],
+        "criteria": [criteria.JAZZ, criteria.INSTRUMENTAL, criteria.UNCOMMON_INSTRUMENTS, criteria.JAZZY_GROOVY, criteria.C_ELECTRO, criteria.ODD_RHYTHM, criteria.SOFT]
     },
     {
         "name": "New Age / Electronic Ambient",
@@ -114,7 +124,8 @@ const subgenres = [
             getAlbumById("anna_sjalv_tredje_tussilago_fanfara"),
             getAlbumById("vangelis_beaubourg"),
             getAlbumById("pythagoras_journey_to_the_vast_unknown")
-        ]
+        ],
+        "criteria": [criteria.ELECTRO, criteria.AMBIENT, criteria.EXPERIMENTAL, criteria.INSTRUMENTAL, criteria.SOFT, criteria.C_PSYCHEDELIC, criteria.SCI_FI, criteria.SOUND_COLLAGES]
     },
     {
         "name": "Neo-Prog",
@@ -124,8 +135,10 @@ const subgenres = [
             getAlbumById("king_crimson_three_of_a_perfect_pair"),
             getAlbumById("low_flying_aircraft_low_flying_aircraft"),
             getAlbumById("camel_nude"),
-            getAlbumById("eskaton_4_visions")
-        ]
+            getAlbumById("eskaton_4_visions"),
+            getAlbumById("kaipa_nattdjurstid")
+        ],
+        "criteria": [criteria.ELECTRO, criteria.C_ELECTRO, criteria.ART_POP, criteria.SOFT, criteria.CONCEPT, criteria.TOUCHING_VOCALS, criteria.NEO_EIGHTIES]
     },
     {
         "name": "Occult / Heavy",
@@ -137,7 +150,8 @@ const subgenres = [
             getAlbumById("sir_lord_baltimore_kingdom_come"),
             getAlbumById("warhorse_warhorse"),
             getAlbumById("aphrodites_child_666")
-        ]
+        ],
+        "criteria": [criteria.OCCULT, criteria.HEAVY, criteria.C_PSYCHEDELIC, criteria.ROCK, criteria.C_FOLK, criteria.JAM]
     },
     {
         "name": "Opera Rock / Fanfare",
@@ -148,9 +162,35 @@ const subgenres = [
             getAlbumById("the_crazy_world_of_arthur_brown_the_crazy_world_of_arthur_brown"),
             getAlbumById("frank_zappa_joes_garage"),
             getAlbumById("roy_wood_mustard"),
-            getAlbumById("wizzard_wizzard_brew")
-            // "jesus christ superstar"
-        ]
+            getAlbumById("wizzard_wizzard_brew"),
+            getAlbumById("andrew_lloyd_webber_and_tim_rice_jesus_christ_superstar"),
+            getAlbumById("la_revolution_francaise_la_revolution_francaise"),
+        ],
+        "criteria": [criteria.ROCK_OPERA, criteria.ART_POP, criteria.BLENDS, criteria.FANFARE, criteria.ORCHESTRAL_PARTS, criteria.BAROQUE, criteria.TRANSITION, criteria.STORYLINE]
+    },
+    {
+        "name": "Progcore / Small Symphonic",
+        "description": "TODO",
+        "mostRepresentativeAlbum": getAlbumById("kaipa_kaipa"),
+        "albums": [
+            getAlbumById("alphataurus_alphataurus"),
+            getAlbumById("camel_moonmadness"),
+            getAlbumById("canamii_concept"),
+            getAlbumById("circus_movin_on"),
+            getAlbumById("emerson_lake_and_palmer_emerson_lake_and_palmer"),
+            getAlbumById("focus_moving_waves"),
+            getAlbumById("fuchsia_fuchsia"),
+            getAlbumById("genesis_a_trick_of_the_tail"),
+            getAlbumById("gnidrolog_lady_lake"),
+            getAlbumById("harmonium_si_on_avait_besoin_dune_cinquieme_saison"),
+            getAlbumById("ikarus_ikarus"),
+            getAlbumById("khan_space_shanty"),
+            getAlbumById("miklagard_miklagard"),
+            getAlbumById("pink_floyd_dark_side_of_the_moon"),
+            getAlbumById("robert_connolly_plateau"),
+            getAlbumById("zyma_brave_new_world"),
+        ],
+        "criteria": [criteria.SYMPHONIC, criteria.SPECTACULAR, criteria.SOPHISTICATED, criteria.TECHNICAL, criteria.C_JAZZ, criteria.C_ELECTRO, criteria.ODD_RHYTHM]
     },
     {
         "name": "Psychedelic",
@@ -165,7 +205,8 @@ const subgenres = [
             getAlbumById("jefferson_airplane_crown_of_creation"),
             getAlbumById("ultimate_spinach_ultimate_spinach"),
             getAlbumById("may_blitz_may_blitz")
-        ]
+        ],
+        "criteria": [criteria.C_PSYCHEDELIC, criteria.AMBIENT, criteria.PATCHWORK, criteria.C_FOLK, criteria.CRISPY_SIXTIES, criteria.SOFT]
     },
     {
         "name": "Renaissance",
@@ -173,7 +214,8 @@ const subgenres = [
         "mostRepresentativeAlbum": getAlbumById("semiramis_dedicato_a_frazz"),
         "albums": [
             getAlbumById("ange_au_dela_du_delire")
-        ]
+        ],
+        "criteria": []
     },
     {
         "name": "Space Rock",
@@ -182,7 +224,8 @@ const subgenres = [
         "albums": [
             getAlbumById("nektar_a_tab_in_the_ocean"),
             getAlbumById("ozric_tentacles_strangeitude"),
-        ]
+        ],
+        "criteria": []
     },
     {
         "name": "Symphonic",
@@ -194,7 +237,8 @@ const subgenres = [
             getAlbumById("van_der_graaf_generator_h_to_he_who_am_the_only_one"),
             getAlbumById("emerson_lake_and_palmer_tarkus"),
             getAlbumById("eloy_ocean")
-        ]
+        ],
+        "criteria": []
     },
     {
         "name": "Zeuhl",
@@ -202,7 +246,8 @@ const subgenres = [
         "mostRepresentativeAlbum": getAlbumById("magma_mekanik_destruktiw_kommandoh"),
         "albums": [
             getAlbumById("weidorje_weidorje")
-        ]
+        ],
+        "criteria": [criteria.ZEUHL, criteria.SPECTACULAR, criteria.ROCK_OPERA, criteria.SCI_FI, criteria.LENGTHY_TRACKS, criteria.CONCEPT, criteria.C_PSYCHEDELIC, criteria.EXPERIMENTAL, criteria.FANFARE, criteria.C_JAZZ, criteria.SYMPHONIC]
     }
     
 ];
