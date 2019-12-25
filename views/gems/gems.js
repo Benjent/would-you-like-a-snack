360b5a1bc3b712b950986a01c25717055d815c40
Benjent <Benjent@hotmail.fr>
2019-12-25 17:37:27 +0100

    Lint gems views

--- views/gems/gems.js
+++ views/gems/gems.js
@@ -9,7 +9,7 @@ const Gems = Vue.component('gems', {
                 <img
                     class="album-cover"
                     :album-id="album.id"
-                    v-on:click="selectAlbumAndView(album)"
+                    @click="selectAlbumAndView(album)"
                     :src=album.cover alt="">
                 
                 <div class="album-infos">
@@ -17,7 +17,7 @@ const Gems = Vue.component('gems', {
                     <span
                         class="album-title"
                         :album-id="album.id"
-                        v-on:click="selectAlbumAndView(album)">
+                        @click="selectAlbumAndView(album)">
                         {{album.artist}} - {{album.title}} ({{album.year}})
                     </span>
                     <blockquote class="album-description">{{album.description}}</blockquote>
